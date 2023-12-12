0f2cbab4573247bb9a5ca250703749a081d574d1
Nico <ni.navarroa@duocuc.cl>
2023-12-12 00:12:11 -0300

    yata

--- src/app/pages/pagina-scan-qr/pagina-scan-qr.page.ts
+++ src/app/pages/pagina-scan-qr/pagina-scan-qr.page.ts
@@ -7,7 +7,7 @@ import { AutentificacionService } from 'src/app/autentificacion.service';
 import { DataService } from 'src/app/services/data.service';
 import { Asistencia } from 'src/app/interfaces/asistencia';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx'
-
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pagina-scan-qr',
@@ -28,12 +28,17 @@ export class PaginaScanQrPage implements OnInit {
   }
   texto: string = ''
 
-  constructor(private database: DataService,
+  palabras: string[] = ["Inglés", "Arquitectura", "Calidad de software", "Portafolio"];
+
+  constructor(
+    private database: DataService,
     private barcodescanner: BarcodeScanner,
     private router: Router,
     private storage: Storage,
     private autentificacionService: AutentificacionService,
-    private firebaseComponent: FirebaseComponent) { }
+    private firebaseComponent: FirebaseComponent,
+    private alertController: AlertController
+  ) { }
 
   ionViewDidEnter() {
     this.obtenerDato();
@@ -48,18 +53,26 @@ export class PaginaScanQrPage implements OnInit {
     this.barcodescanner.scan().then(barcodedata => {
       console.log("Scaneando...", barcodedata);
       if (userData && userData.state && userData.state.user && userData.state.user.email) {
-        this.qr = barcodedata.text
-        this.texto = (JSON.stringify(barcodedata));
+        this.qr = barcodedata.text;
+        this.texto = JSON.stringify(barcodedata);
         const userEmail = userData.state.user.email;
         console.log('Correo:', userEmail);
         const id = this.database.getID();
         this.nuevaAsis.estudiante = userEmail;
         this.nuevaAsis.Asignatura = this.qr;
-        this.database.crearDoc(this.nuevaAsis, this.path, id)
-        this.database.guardarDato('Asistencia Stoarge',);
-        this.obtenerDato();
-        if (this.nuevaAsis.Asignatura.trim() !== '') {
+
+        // Verificar si la asignatura está en la lista de palabras
+        const asignaturaValida = this.palabras.includes(this.nuevaAsis.Asignatura.trim());
+
+        if (asignaturaValida) {
+          // Asignatura válida, guardar datos y navegar a asignaturas
+          this.database.crearDoc(this.nuevaAsis, this.path, id)
+          this.database.guardarDato('Asistencia Stoarge');
+          this.obtenerDato();
           this.router.navigate(['/asignaturas'], { state: { showAlert: true } });
+        } else {
+          // Asignatura no válida, puedes manejar esto según tus requisitos
+          this.alertadescan()
         }
       } else {
         console.error('Nota el correo ql');
@@ -68,13 +81,19 @@ export class PaginaScanQrPage implements OnInit {
     }).catch(err => {
       console.log("ERROR AL ESCANEAR!!!!");
     })
-
-
   }
+
   ngOnInit() {
 
   }
-
-
+  async alertadescan(){
+    const alert = await this.alertController.create({
+      header: "Problemas QR",
+      message: "Asegurate de escanear el QR del profesor y no cualquiera",
+      buttons: ["OK"]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
 }
-
